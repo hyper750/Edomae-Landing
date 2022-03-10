c8cd7cbaac98eacacc891b13932a29f6a95b20c9
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-03-10 19:32:05 +0100

    Increased left padding for nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light px-2">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ps-3">
         <router-link to="/">
             <b-img
                 :src="require('../assets/edomae.png')"

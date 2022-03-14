5b0760a5cd89150109f53b8035515de44bf35121
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-03-14 18:16:53 +0100

    Load meal category

--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -6,6 +6,12 @@
             </b-col>
         </b-row>
 
+        <b-row class="px-3">
+            <b-col md="12" class="bg-white p-4">
+                
+            </b-col>
+        </b-row>
+
         <b-row class="mt-4">
             <b-col md="12">
                 <FooterBar />
@@ -17,11 +23,39 @@
 <script>
 import NavBar from "../components/NavBar";
 import FooterBar from "../components/FooterBar";
+import MealEndpoints from "../axios/api/meal";
+import MealCategoryEndpoints from "../axios/api/mealCategory";
 
 export default {
     components: {
         NavBar,
         FooterBar,
+    },
+
+    mounted() {
+        this.loadMealCategory();
+        this.loadMeal();
+    },
+
+    data() {
+        return {
+            meal: [],
+            mealCategory: [],
+        };
+    },
+
+    methods: {
+        loadMealCategory() {
+            MealCategoryEndpoints.list({
+                enabled: true,
+            }).then(({ data }) => (this.mealCategory = data));
+        },
+
+        loadMeal() {
+            MealEndpoints.list({
+                enabled: true,
+            }).then(({ data }) => (this.meal = data));
+        },
     },
 };
 </script>

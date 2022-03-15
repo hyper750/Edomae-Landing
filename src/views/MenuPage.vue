3d4d86b76996ea8e48673b359962410d039131db
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-03-15 18:08:55 +0100

    Show meals

--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -9,6 +9,14 @@
         <b-row class="px-3">
             <b-col md="12" class="bg-white p-4">
                 <b-container fluid>
+                    <b-row>
+                        <b-col md="12">
+                            <h2 class="text-center text-muted mb-4">
+                                {{ $t("Menu") }}
+                            </h2>
+                        </b-col>
+                    </b-row>
+
                     <!-- Show categories if no category is selected -->
                     <b-row v-if="!selectedCategory">
                         <b-col
@@ -30,7 +38,38 @@
                         </b-col>
                     </b-row>
                     <!-- Show meals of the category selected -->
-                    <b-row v-if="selectedCategory"> </b-row>
+                    <b-row v-if="selectedCategory">
+                        <b-col md="12" class="mb-3">
+                            <b-btn
+                                variant="outline-primary"
+                                class="me-4"
+                                @click="() => unselectCategory()"
+                            >
+                                <BIconChevronLeft />
+                                {{ $t("Go to meal") }}
+                            </b-btn>
+                            <h3 class="d-inline align-middle">
+                                {{ selectedCategory.name }}
+                            </h3>
+                        </b-col>
+
+                        <b-col
+                            md="3"
+                            v-for="meal in getMealByCategory"
+                            :key="meal.id"
+                        >
+                            <b-card
+                                :title="meal.name"
+                                :img-src="meal.imatge"
+                                :img-alt="meal.name"
+                                class="mb-4 menu-card shadow-sm"
+                                overlay
+                                text-variant="white"
+                                img-top
+                            >
+                            </b-card>
+                        </b-col>
+                    </b-row>
                 </b-container>
             </b-col>
         </b-row>
@@ -48,11 +87,13 @@ import NavBar from "../components/NavBar";
 import FooterBar from "../components/FooterBar";
 import MealEndpoints from "../axios/api/meal";
 import MealCategoryEndpoints from "../axios/api/mealCategory";
+import { BIconChevronLeft } from "bootstrap-vue";
 
 export default {
     components: {
         NavBar,
         FooterBar,
+        BIconChevronLeft,
     },
 
     mounted() {
@@ -67,6 +108,14 @@ export default {
 
             selectedCategory: null,
         };
+    },
+
+    computed: {
+        getMealByCategory() {
+            return this.meals.filter(
+                ({ category }) => category === this.selectedCategory.id
+            );
+        },
     },
 
     methods: {
@@ -84,6 +133,10 @@ export default {
 
         selectCategory(mealCategory) {
             this.selectedCategory = mealCategory;
+        },
+
+        unselectCategory() {
+            this.selectedCategory = null;
         },
     },
 };

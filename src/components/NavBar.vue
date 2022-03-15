d68034008b859c04d6abc8b22078dc7441ace909
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-03-15 18:04:10 +0100

    Updated navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ps-3">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary ps-3">
         <router-link to="/">
             <b-img
                 :src="require('../assets/edomae.png')"

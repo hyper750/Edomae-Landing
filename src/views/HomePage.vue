a7978d6cc2c6f573180d0e396bf70e9cdb370541
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-03-23 22:23:48 +0100

    Uploaded carousel sample

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,6 +6,38 @@
             </b-col>
         </b-row>
 
+        <b-row class="px-3">
+            <b-col md="12" class="bg-white p-4">
+                <b-container fluid>
+                    <b-row>
+                        <b-col md="12">
+                            <h2 class="text-center">
+                                {{ $t("Your trusted Japanese restaurant") }}
+                            </h2>
+                        </b-col>
+                        <b-col md="12 mt-3">
+                            <b-carousel
+                                id="home-carousel"
+                                v-model="carouselSlide"
+                                :interval="4000"
+                                controls
+                                indicators
+                                img-width="1024"
+                                img-height="400"
+                                style="text-shadow: 1px 1px 2px #333"
+                            >
+                                <b-carousel-slide
+                                    v-for="image in imageSlides"
+                                    :key="image.path"
+                                    :img-src="image.path"
+                                />
+                            </b-carousel>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </b-col>
+        </b-row>
+
         <b-row class="mt-4">
             <b-col md="12">
                 <FooterBar />
@@ -22,6 +54,48 @@ export default {
     components: {
         NavBar,
         FooterBar,
+    },
+
+    data() {
+        return {
+            carouselSlide: 0,
+
+            imageSlides: [
+                {
+                    path: require("../assets/carousel/local/IMG_20220318_165348.jpg"),
+                },
+                {
+                    path: require("../assets/carousel/local/IMG_20220318_165622.jpg"),
+                },
+                {
+                    path: require("../assets/carousel/local/IMG_20220318_165606.jpg"),
+                },
+                {
+                    path: require("../assets/carousel/local/IMG_20220318_165455.jpg"),
+                },
+                {
+                    path: require("../assets/carousel/local/IMG_20220318_165330.jpg"),
+                },
+                {
+                    path: require("../assets/carousel/meal/yakisoba.jpeg"),
+                },
+                {
+                    path: require("../assets/carousel/meal/sushi box.jpeg"),
+                },
+                {
+                    path: require("../assets/carousel/meal/IMG_20211020_205937_1.jpg"),
+                },
+                {
+                    path: require("../assets/carousel/meal/udon picante.jpg"),
+                },
+                {
+                    path: require("../assets/carousel/meal/IMG_20211020_211357_1.jpg"),
+                },
+                {
+                    path: require("../assets/carousel/meal/daifuku.jpeg"),
+                },
+            ],
+        };
     },
 };
 </script>

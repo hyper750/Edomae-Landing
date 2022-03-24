fb4f036b70633cfc5a7f53635b1979be1ed6fcff
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-03-25 00:20:17 +0100

    Added example of home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,29 +9,23 @@
         <b-row class="px-3">
             <b-col md="12" class="bg-white p-4">
                 <b-container fluid>
-                    <b-row>
-                        <b-col md="12">
-                            <h2 class="text-center">
-                                {{ $t("Your trusted Japanese restaurant") }}
-                            </h2>
+                    <b-row v-for="(image, index) in imageSlides" :key="index" class="mb-4">
+                        <!-- Text left for even -->
+                        <b-col md="3" class="align-self-center" v-if="index % 2 == 0">
+                            <h2 class="text-center">{{ $t(image.title) }}</h2>
+                            <h5 class="text-secondary text-center">{{ $t(image.subtitle) }}</h5>
                         </b-col>
-                        <b-col md="12 mt-3">
-                            <b-carousel
-                                id="home-carousel"
-                                v-model="carouselSlide"
-                                :interval="4000"
-                                controls
-                                indicators
-                                img-width="1024"
-                                img-height="400"
-                                style="text-shadow: 1px 1px 2px #333"
-                            >
-                                <b-carousel-slide
-                                    v-for="image in imageSlides"
-                                    :key="image.path"
-                                    :img-src="image.path"
-                                />
-                            </b-carousel>
+                        <b-col md="9" v-if="index % 2 == 0">
+                            <b-img :src="image.path" class="w-100"/>
+                        </b-col>
+
+                        <!-- Text right for odd -->
+                        <b-col md="9" v-if="index % 2 == 1">
+                            <b-img :src="image.path" class="w-100"/>
+                        </b-col>
+                        <b-col md="3" class="align-self-center" v-if="index % 2 == 1">
+                            <h2 class="text-center">{{ $t(image.title) }}</h2>
+                            <h5 class="text-secondary text-center">{{ $t(image.subtitle) }}</h5>
                         </b-col>
                     </b-row>
                 </b-container>
@@ -58,41 +52,61 @@ export default {
 
     data() {
         return {
-            carouselSlide: 0,
-
             imageSlides: [
                 {
-                    path: require("../assets/carousel/local/IMG_20220318_165348.jpg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/local/IMG_20220318_165348.jpg"),
                 },
                 {
-                    path: require("../assets/carousel/local/IMG_20220318_165622.jpg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/local/IMG_20220318_165622.jpg"),
                 },
                 {
-                    path: require("../assets/carousel/local/IMG_20220318_165606.jpg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/local/IMG_20220318_165606.jpg"),
                 },
                 {
-                    path: require("../assets/carousel/local/IMG_20220318_165455.jpg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/local/IMG_20220318_165455.jpg"),
                 },
                 {
-                    path: require("../assets/carousel/local/IMG_20220318_165330.jpg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/local/IMG_20220318_165330.jpg"),
                 },
                 {
-                    path: require("../assets/carousel/meal/yakisoba.jpeg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/meal/yakisoba.jpeg"),
                 },
                 {
-                    path: require("../assets/carousel/meal/sushi box.jpeg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/meal/sushi box.jpeg"),
                 },
                 {
-                    path: require("../assets/carousel/meal/IMG_20211020_205937_1.jpg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/meal/IMG_20211020_205937_1.jpg"),
                 },
                 {
-                    path: require("../assets/carousel/meal/udon picante.jpg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/meal/udon picante.jpg"),
                 },
                 {
-                    path: require("../assets/carousel/meal/IMG_20211020_211357_1.jpg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/meal/IMG_20211020_211357_1.jpg"),
                 },
                 {
-                    path: require("../assets/carousel/meal/daifuku.jpeg"),
+                    title: 'Your trusted Japanese restaurant',
+                    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra ut mauris a dignissim. Nunc sit amet ante scelerisque, vulputate nunc eget, viverra lorem. Etiam id ultrices velit. ',
+                    path: require("../assets/photos/meal/daifuku.jpeg"),
                 },
             ],
         };
